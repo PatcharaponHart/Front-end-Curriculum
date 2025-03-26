e54bbd3ba30ceb010a0492bc6956be9820a7bc3e
Natchayan <natchayan.t@ku.th>
2025-03-27 00:48:27 +0700

    <update> CourseStructure

--- src/components/CourseStructure.vue
+++ src/components/CourseStructure.vue
@@ -26,12 +26,13 @@
                 <span>ไม่น้อยกว่า 3 หน่วยกิต</span>
             </li>
             <li>
-                <span class="long-text" style="margin-right: 95px">
+                <span style="margin-right: 95px">
                     เลือกเรียนรายวิชาใน 5 กลุ่มสาระของหมวดวิชาศึกษาทั่วไปเฉพาะที่เป็น
                     <br />รายวิชาของคณะต้นสังกัดของหลักสูตรในแต่ละวิทยาเขตอีกไม่น้อยกว่า <br />5 หน่วยกิต ยกเว้นกลุ่มสาระภาษาและการสื่อสาร
                 </span>
                 <span>ไม่น้อยกว่า 5 หน่วยกิต</span>
             </li>
+            <br />
         </ul>
 
         <p><strong class="me-2">2. หมวดวิชาเฉพาะ</strong> <span>ไม่น้อยกว่า 88 หน่วยกิต</span></p>
@@ -50,6 +51,7 @@
                 <span> ไม่น้อยกว่า 18 หน่วยกิต</span>
             </li>
         </ul>
+        <br />
 
         <p><strong class="me-2">3.หมวดวิชาเลือกเสรี </strong> <span>ไม่น้อยกว่า 6 หน่วยกิต</span></p>
     </div>
@@ -69,7 +71,401 @@
                 <span style="margin-right: 130px">01175xxx กิจกรรมพลศึกษา (Physical Education Activities)</span>
                 <span>1(0-2-1) หน่วยกิต</span>
             </li>
+
             <p><span>และเลือกเรียนอีกไม่น้อยกว่า 2 หน่วยกิต จากรายวิชาในหมวดวิชาศึกษาทั่วไป กลุ่มสาระอยู่ดีมีสุข</span></p>
+
+            <li>
+                <span style="margin-right: 360px">กลุ่มสาระศาสตร์แห่งผู้ประกอบการ </span>
+                <span>ไม่น้อยกว่า 3 หน่วยกิต</span>
+            </li>
+
+            <p><span>เลือกเรียนไม่น้อยกว่า 3 หน่วยกิต จากรายวิชาในหมวดวิชาศึกษาทั่วไป กลุ่มสาระศาสตร์แห่งผู้ประกอบการ</span></p>
+
+            <li>
+                <span style="margin-right: 400px">กลุ่มสาระภาษากับการสื่อสาร </span>
+                <span>ไม่น้อยกว่า 13 หน่วยกิต</span>
+            </li>
+
+            <li>
+                <span style="margin-right: 507px">วิชาภาษาไทย </span>
+                <span>3( – – ) หน่วยกิต</span>
+            </li>
+
+            <li>
+                <span style="margin-right: 393px">วิชาภาษาต่างประเทศ 1 ภาษา </span>
+                <span>9( – – ) หน่วยกิต</span>
+            </li>
+
+            <li>
+                <span style="margin-right: 405px">วิชาสารสนเทศ/คอมพิวเตอร์ </span>
+                <span>1( – – ) หน่วยกิต</span>
+            </li>
+
+            <li>
+                <span style="margin-right: 325px">กลุ่มสาระพลเมืองไทยและพลเมืองโลก </span>
+                <span>ไม่น้อยกว่า 3 หน่วยกิต</span>
+            </li>
+
+            <li class="arrow-item">
+                <span style="margin-right: 163px">01999111 ศาสตร์แห่งแผ่นดิน (Knowledge of the Land)</span>
+                <span>2(2-0-4) หน่วยกิต</span>
+            </li>
+
+            <p><span>และเลือกเรียนอีกไม่น้อยกว่า 1 หน่วยกิต จากรายวิชาในหมวดวิชาศึกษาทั่วไป กลุ่มสาระพลเมืองไทยและพลเมืองโลก</span></p>
+
+            <li>
+                <span style="margin-right: 438px">กลุ่มสาระสุนทรียศาสตร์ </span>
+                <span>ไม่น้อยกว่า 3 หน่วยกิต </span>
+            </li>
+
+            <p>
+                <span>เลือกเรียนไม่น้อยกว่า 3 หน่วยกิต จากรายวิชาในหมวดวิชาศึกษาทั่วไป กลุ่มสาระสุนทรียศาสตร์<br />และเลือกเรียนรายวิชาในหมวดวิชาศึกษาทั่วไปอีกไม่น้อยกว่า 5 หน่วยกิต</span>
+            </p>
+            <br />
+
+            <p><strong class="me-2">2.หมวดวิชาเฉพาะ ไม่น้อยกว่า 88</strong> <span>ไม่น้อยกว่า 30 หน่วยกิต</span></p>
+
+            <li>
+                <span style="margin-right: 550px">วิชาแกน </span>
+                <span>12 หน่วยกิต </span>
+            </li>
+
+            <li class="arrow-item">
+                <span style="margin-right: 331px">01417111 แคลคูลัส I (Calculus I)</span>
+                <span>3(3-0-6) หน่วยกิต</span>
+            </li>
+
+            <li class="arrow-item">
+                <span style="margin-right: 151px">01417322 พีชคณิตเชิงเส้นพื้นฐาน (Basic Linear Algebra)</span>
+                <span>3(3-0-6) หน่วยกิต</span>
+            </li>
+
+            <li class="arrow-item">
+                <span style="margin-right: 135px">01418131 การโปรแกรมทางสถิติ (Statistical Programming)</span>
+                <span>3(3-0-6) หน่วยกิต</span>
+            </li>
+
+            <li class="arrow-item">
+                <span style="margin-right: 130px">01418132 หลักมูลการคณนา (Fundamentals of Computing)</span>
+                <span>3(3-0-6) หน่วยกิต</span>
+            </li>
+
+            <li>
+                <span style="margin-right: 495px">วิชาเฉพาะบังคับ </span>
+                <span>58 หน่วยกิต </span>
+            </li>
+            <br />
+
+            <p><span>กลุ่มประเด็นด้านองค์การและระบบสารสนเทศ</span></p>
+
+            <li class="arrow-item">
+                <span style="margin-right: 180px">01418341 ทรัพย์สินทางปัญญาและจรรยาบรรณวิชาชีพ<br />(Intellectual Properties and Professional Ethics)</span>
+                <span>3(3-0-6) หน่วยกิต</span>
+            </li>
+
+            <li class="arrow-item">
+                <span style="margin-right: 208px">01418371 การบริหารโครงการและสตาร์ทอัพดิจิทัล<br />(Project Management and Digital Startup)</span>
+                <span>3(3-0-6) หน่วยกิต</span>
+            </li>
+            <br />
+
+            <p><span>กลุ่มเทคโนโลยีเพื่องานประยุกต์</span></p>
+
+            <li class="arrow-item">
+                <span style="margin-right: 295px"
+                    >01418221 ระบบฐานข้อมูลเบื้องต้น<br />
+                    (Fundamentals of Database Systems)</span
+                >
+                <span>3(3-0-6) หน่วยกิต</span>
+            </li>
+
+            <li class="arrow-item">
+                <span style="margin-right: 267px"
+                    >01418261 หลักพื้นฐานของปัญญาประดิษฐ์<br />
+                    (Fundamentals of Artificial Intelligence)</span
+                >
+                <span>3(3-0-6) หน่วยกิต</span>
+            </li>
+
+            <li class="arrow-item">
+                <span style="margin-right: 210px">01418321 การวิเคราะห์และการออกแบบระบบ<br />(Intellectual Properties and Professional Ethics)</span>
+                <span>3(2-2-5) หน่วยกิต</span>
+            </li>
+
+            <li class="arrow-item">
+                <span style="margin-right: 254px">01418390 การเตรียมความพร้อมสหกิจศึกษา<br />(Project Management and Digital Startup)</span>
+                <span>1(1-0-2) หน่วยกิต</span>
+            </li>
+
+            <li class="arrow-item">
+                <span style="margin-right: 215px">01418490 สหกิจศึกษา (Cooperative Education)</span>
+                <span>6 หน่วยกิต</span>
+            </li>
+
+            <li class="arrow-item">
+                <span style="margin-right: 366px">01418497 สัมมนา (Seminar)</span>
+                <span>1 หน่วยกิต</span>
+            </li>
+
+            <li class="arrow-item">
+                <span style="margin-right: 53px">01418499 โครงงานวิทยาการคอมพิวเตอร์ (Computer Science Project)</span>
+                <span>3(0-9-5) หน่วยกิต</span>
+            </li>
+            <br />
+
+            <p><span>กลุ่มเทคโนโลยีและวิธีการทางซอฟต์แวร์</span></p>
+
+            <li class="arrow-item">
+                <span style="margin-right: 281px">01418112 แนวคิดการโปรแกรมเบื้องต้น<br />(Fundamental Programming Concepts)</span>
+                <span>3(2-2-5) หน่วยกิต</span>
+            </li>
+
+            <li class="arrow-item">
+                <span style="margin-right: 105px">01418113 การโปรแกรมคอมพิวเตอร์ (Computer Programming)</span>
+                <span>3(2-2-5) หน่วยกิต</span>
+            </li>
+
+            <li class="arrow-item">
+                <span style="margin-right: 164px">01418211 การสร้างซอฟต์แวร์ (Software Construction)</span>
+                <span>3(2-2-5) หน่วยกิต</span>
+            </li>
+
+            <li class="arrow-item">
+                <span style="margin-right: 277px">01418231 โครงสร้างข้อมูลและขั้นตอนวิธี<br />(Data Structures and Algorithms)</span>
+                <span>3(3-0-6) หน่วยกิต</span>
+            </li>
+
+            <li class="arrow-item">
+                <span style="margin-right: 210px">01418232 การออกแบบและการวิเคราะห์ขั้นตอนวิธี<br />(Algorithm Design and Analysis)</span>
+                <span>3(3-0-6) หน่วยกิต</span>
+            </li>
+            <br />
+
+            <p><span>กลุ่มโครงสร้างพื้นฐานของระบบ</span></p>
+
+            <li class="arrow-item">
+                <span style="margin-right: 280px">01418111 วิทยาการคอมพิวเตอร์เบื้องต้น<br />(Introduction to Computer Science)</span>
+                <span>2(2-0-4) หน่วยกิต</span>
+            </li>
+
+            <li class="arrow-item">
+                <span style="margin-right: 222px">01418236 ระบบปฏิบัติการ (Operating Systems)</span>
+                <span>3(3-0-6) หน่วยกิต</span>
+            </li>
+
+            <li class="arrow-item">
+                <span style="margin-right: 168px">01418331 ทฤษฎีการคำนวณ (Theory of Computation)</span>
+                <span>3(3-0-6) หน่วยกิต</span>
+            </li>
+
+            <li class="arrow-item">
+                <span style="margin-right: 279px">01418332 ความมั่นคงในระบบสารสนเทศ<br />(Information Systems Security)</span>
+                <span>3(3-0-6) หน่วยกิต</span>
+            </li>
+
+            <li class="arrow-item">
+                <span style="margin-right: 57px">01418351 หลักการการสื่อสารคอมพิวเตอร์และการประมวลผลบนคลาวด์<br />(Computer Communications and Cloud Computing Principles)</span>
+                <span>3(3-0-6) หน่วยกิต</span>
+            </li>
+            <br />
+
+            <p><span>กลุ่มฮาร์ดแวร์และสถาปัตยกรรมคอมพิวเตอร์</span></p>
+
+            <li class="arrow-item">
+                <span style="margin-right: 106px">01418233 สถาปัตยกรรมคอมพิวเตอร์ (Computer Architecture)</span>
+                <span>3(3-0-6) หน่วยกิต</span>
+            </li>
+
+            <li>
+                <span style="margin-right: 500px">วิชาเฉพาะเลือก </span>
+                <span>ไม่น้อยกว่า 18 หน่วยกิต</span>
+            </li>
+            <br />
+
+            <p><span>ให้เลือกเรียนรายวิชาในสาขาวิชาวิทยาการคอมพิวเตอร์ ไม่น้อยกว่า 18 หน่วยกิต โดยเป็นวิชา 014182xx ขึ้นไป</span></p>
+
+            <li class="arrow-item">
+                <span style="margin-right: 209px">01418212 การโปรแกรมภาษาซี (C Programming)</span>
+                <span>3(2-2-5) หน่วยกิต</span>
+            </li>
+
+            <li class="arrow-item">
+                <span style="margin-right: 118px">01418213 การโปรแกรมภาษาโคบอล (COBOL Programming)</span>
+                <span>3(2-2-5) หน่วยกิต</span>
+            </li>
+
+            <li class="arrow-item">
+                <span style="margin-right: 252px">01418214 การฝึกปฏิบัติการพัฒนาซอฟต์แวร์<br />(Practicum in Software Development)</span>
+                <span>1(0-3-2) หน่วยกิต</span>
+            </li>
+
+            <li class="arrow-item">
+                <span style="margin-right: 59px">01418222 ระบบสารสนเทศวิสาหกิจ (Enterprise Information System)</span>
+                <span> 3(3-0-6) หน่วยกิต</span>
+            </li>
+
+            <li class="arrow-item">
+                <span style="margin-right: 227px">01418233 วิทยาการข้อมูลและโปรแกรมประยุกต์ <br />(Data Science and Application Programs)</span>
+                <span>3(2-2-5) หน่วยกิต</span>
+            </li>
+
+            <li class="arrow-item">
+                <span style="margin-right: 224px">01418234 การโปรแกรมอินเทอร์เน็ตของสรรพสิ่ง <br />(Programming Internet of Things)</span>
+                <span>3(2-2-5) หน่วยกิต</span>
+            </li>
+
+            <li class="arrow-item">
+                <span style="margin-right: 126px">01418235 ระบบปฏิบัติการยูนิกซ์และการโปรแกรมเปลือกระบบ <br />(Unix Operating System and Shell Programming)</span>
+                <span>3(2-2-5) หน่วยกิต</span>
+            </li>
+
+            <li class="arrow-item">
+                <span style="margin-right: 168px">01418241 เทคโนโลยีสารสนเทศการเงินและการธนาคาร <br />(Financial and Banking Information Technology)</span>
+                <span>3(2-2-5) หน่วยกิต</span>
+            </li>
+
+            <li class="arrow-item">
+                <span style="margin-right: 165px">01418281 หลักการสร้างภาพเคลื่อนไหวด้วยคอมพิวเตอร์ <br />(Principles of Computer Animation)</span>
+                <span>3(3-0-6) หน่วยกิต</span>
+            </li>
+
+            <li class="arrow-item">
+                <span style="margin-right: 271px">01418282 การประมวลผลภาพและวีดิทัศน์ <br />(Image and Video Processing)</span>
+                <span>3(3-0-6) หน่วยกิต</span>
+            </li>
+
+            <li class="arrow-item">
+                <span style="margin-right: 232px">01418311 การโปรแกรมเชิงคำนวณแบบท้าทาย <br />(Challenging Computational Programming)</span>
+                <span>3(2-2-5) หน่วยกิต</span>
+            </li>
+
+            <li class="arrow-item">
+                <span style="margin-right: 84px">01418322 วิทยาการข้อมูลเบื้องต้น (Introduction to Data Science)</span>
+                <span> 3(2-2-5) หน่วยกิต</span>
+            </li>
+
+            <li class="arrow-item">
+                <span style="margin-right: 93px">01418323 การจัดการคุณภาพข้อมูล (Data Quality Management)</span>
+                <span> 3(3-0-6) หน่วยกิต</span>
+            </li>
+
+            <li class="arrow-item">
+                <span style="margin-right: 144px">01418324 ระบบสนับสนุนการตัดสินใจและอัจฉริยะทางธุรกิจ <br />(Decision Support and Business Intelligent Systems)</span>
+                <span>3(3-0-6) หน่วยกิต</span>
+            </li>
+
+            <li class="arrow-item">
+                <span style="margin-right: 226px">01418325 ข้อมูลจินตทัศน์ (Data Visualization)</span>
+                <span> 3(2-2-5) หน่วยกิต</span>
+            </li>
+
+            <li class="arrow-item">
+                <span style="margin-right: 142px">01418333 เทคนิคตัวแปลโปรแกรม (Compiler Techniques)</span>
+                <span> 3(3-0-6) หน่วยกิต</span>
+            </li>
+
+            <li class="arrow-item">
+                <span style="margin-right: 50px">01418333 การออกแบบและการพัฒนาระบบการวางแผนทรัพยากรองค์กร <br />(Enterprise Resource Planning System Design and Development)</span>
+                <span>3(2-2-5) หน่วยกิต</span>
+            </li>
+
+            <li class="arrow-item">
+                <span style="margin-right: 89px">01418333 การออกแบบและพัฒนาโปรแกรมประยุกต์สำหรับอุปกรณ์ <br />เคลื่อนที่ (Mobile Application Design and Development)</span>
+                <span>3(2-2-5) หน่วยกิต</span>
+            </li>
+
+            <li class="arrow-item">
+                <span style="margin-right: 277px">01418343 การคำนวณแบบขนานด้วยคูด้า <br />(Parallel Computing with CUDA)</span>
+                <span>3(3-0-6) หน่วยกิต</span>
+            </li>
+
+            <li class="arrow-item">
+                <span style="margin-right: 172px">01418344 การจัดการมิติข้อมูลและรายงานทางธุรกิจ <br />(Business Data Dimension and Report Management)</span>
+                <span>3(3-0-6) หน่วยกิต</span>
+            </li>
+
+            <li class="arrow-item">
+                <span style="margin-right: 280px">01418352 การสื่อสารข้อมูลและเครือข่าย <br />(Data Communications and Networks)</span>
+                <span>3(3-0-6) หน่วยกิต</span>
+            </li>
+
+            <li class="arrow-item">
+                <span style="margin-right: 200px">01418353 แนวคิดและบริการการคำนวณแบบคลาวด์ <br />(Cloud Computing Concepts and Services)</span>
+                <span>3(3-0-6) หน่วยกิต</span>
+            </li>
+
+            <li class="arrow-item">
+                <span style="margin-right: 303px">01418361 คอมพิวเตอร์วิทัศน์เบื้องต้น <br />(Introduction to Computer Vision)</span>
+                <span>3(3-0-6) หน่วยกิต</span>
+            </li>
+
+            <li class="arrow-item">
+                <span style="margin-right: 291px">01418362 การเรียนรู้ของเครื่องเบื้องต้น <br />(Introduction to Machine Learning)</span>
+                <span>3(3-0-6) หน่วยกิต</span>
+            </li>
+
+            <li class="arrow-item">
+                <span style="margin-right: 279px">01418363 การประมวลผลภาษาธรรมชาติ <br />(Natural Language Processing)</span>
+                <span>3(3-0-6) หน่วยกิต</span>
+            </li>
+
+            <li class="arrow-item">
+                <span style="margin-right: 205px">01418381 คอมพิวเตอร์กราฟิกส์เชิงโต้ตอบเบื้องต้น <br />(Introduction to Interactive Computer Graphics)</span>
+                <span>3(3-0-6) หน่วยกิต</span>
+            </li>
+
+            <li class="arrow-item">
+                <span style="margin-right: 265px">01418382 วิชวลเอฟเฟกส์ (Visual Effects)</span>
+                <span> 3(3-0-6) หน่วยกิต</span>
+            </li>
+
+            <li class="arrow-item">
+                <span style="margin-right: 240px">01418383 ความจริงขยาย (Extended Reality)</span>
+                <span> 3(3-0-6) หน่วยกิต</span>
+            </li>
+
+            <li class="arrow-item">
+                <span style="margin-right: 122px">01418421 การออกแบบประสบการณ์และส่วนเชื่อมประสานผู้ใช้ <br />(User Experience and User Interface Design)</span>
+                <span>3(2-2-5) หน่วยกิต</span>
+            </li>
+
+            <li class="arrow-item">
+                <span style="margin-right: 287px">01418442 เว็บเทคโนโลยีและเว็บบริการ <br />(Web Technology and Web Services)</span>
+                <span>3(2-2-5) หน่วยกิต</span>
+            </li>
+
+            <li class="arrow-item">
+                <span style="margin-right: 232px">01418451 การออกแบบและการบริหารเครือข่าย <br />(Network Design and Administration)</span>
+                <span>3(2-2-5) หน่วยกิต</span>
+            </li>
+
+            <li class="arrow-item">
+                <span style="margin-right: 250px"
+                    >01418471 การออกแบบและพัฒนาซอฟต์แวร์ <br />
+                    (Software Design and Development)</span
+                >
+                <span>3(3-0-6) หน่วยกิต</span>
+            </li>
+
+            <li class="arrow-item">
+                <span style="margin-right: 141px">01418472 การบูรณาการกระบวนการเชิงอไจล์และเดฟอ็อปส์ <br />(Integrated Agile Process and DevOps)</span>
+                <span>3(3-0-6) หน่วยกิต</span>
+            </li>
+
+            <li class="arrow-item">
+                <span style="margin-right: 168px">01418473 การควบคุมและการตรวจสอบงานคอมพิวเตอร์<br />(Computer Control and Audit)</span>
+                <span>3(3-0-6) หน่วยกิต</span>
+            </li>
+
+            <li class="arrow-item">
+                <span style="margin-right: 241px">01418474 การทดสอบและทวนสอบซอฟต์แวร์ <br />(Software Testing and Verification)</span>
+                <span>3(3-0-6) หน่วยกิต</span>
+            </li>
+
+            <li class="arrow-item">
+                <span style="margin-right: 227px">01418496 เรื่องเฉพาะทางวิทยาการคอมพิวเตอร์ <br />(Selected Topic in Computer Science)</span>
+                <span>3 หน่วยกิต</span>
+            </li>
         </ul>
     </div>
 </template>
